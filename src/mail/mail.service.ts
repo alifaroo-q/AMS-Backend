7cdce2a48192aaff9a8db378fcde371ceb213d45
Ali Farooq <alifarooq122@gmail.com>
2024-05-20 23:01:11 +0500

    refactored mail service to use sendGrid api

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,18 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { MailerService } from '@nestjs-modules/mailer';
-import { auth } from '@googleapis/oauth2';
-import { Options } from 'nodemailer/lib/smtp-transport';
 
 import * as SendGrid from '@sendgrid/mail';
 
-const EMAIL_CONFIRMATION_TEMPLATE: string =
-  'd-4759f0a5731b4f2888f92ff24843b719';
+const EMAIL_CONFIRMATION_TEMPLATE = 'd-4759f0a5731b4f2888f92ff24843b719';
+const PASSWORD_CHANGE_TEMPLATE = 'd-8fb05ae60f064c408b20b25df31101a7';
 
 @Injectable()
 export class MailService {
   private logger = new Logger('MailService');
 
-  constructor(private readonly mailerService: MailerService) {
+  constructor() {
     SendGrid.setApiKey(process.env.SENDGRID_API_KEY);
   }
 
@@ -24,92 +21,11 @@ export class MailService {
     }
   }
 
-  public async sendTestMail() {
-    return await this.sendGridMail({
-      to: 'alifarooq122@gmail.com',
-      subject: 'testing',
-      from: 'se201027@dsu.edu.pk',
-      templateId: 'd-4759f0a5731b4f2888f92ff24843b719',
-      text: 'Test mail',
-      html: 'Verify email',
-      dynamicTemplateData: {
-        token: 'fslkfsjfkls',
-        domain: 'dsu.edu.pk',
-        username: 'test',
-        link: 'dsu.edu.pk',
-      },
-    });
-  }
-
-  private async setTransport() {
-    const OAuth2 = auth.OAuth2;
-    const oauth2Client = new OAuth2(
-      process.env.GMAIL_CLIENT_ID,
-      process.env.GMAIL_CLIENT_SECRET,
-      'https://developers.google.com/oauthplayground',
-    );
-
-    oauth2Client.setCredentials({
-      refresh_token: process.env.GMAIL_REFRESH_TOKEN,
-    });
-
-    const accessToken: string = await new Promise((resolve, reject) => {
-      oauth2Client.getAccessToken((err, token) => {
-        if (err) {
-          //console.log(err);
-          reject('Opps Failed to create access token');
-        }
-        resolve(token);
-      });
-    });
-
-    const config: Options = {
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: process.env.GMAIL_EMAIL,
-        clientId: process.env.GMAIL_CLIENT_ID,
-        clientSecret: process.env.GMAIL_CLIENT_SECRET,
-        accessToken,
-      },
-    };
-    this.mailerService.addTransporter('gmail', config);
-  }
-
-  public async sendMail(to: string) {
-    try {
-      await this.setTransport();
-    } catch (err) {
-      console.log(err);
-      console.log('Email HALT');
-      return;
-    }
-    this.mailerService
-      .sendMail({
-        transporterName: 'gmail',
-        to: 'se201003@dsu.edu.pk,gosaad@outlook.com', //to,
-        from: 'DSU Alumni Portal <no-reply@dsu.edu.pk>',
-        subject: 'Testing Nest Mailermodule with template ✔',
-        template: 'welcome', // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
-        context: {
-          // Data to be sent to template engine.
-          code: 'cf1a3f828287',
-          username: 'john doe',
-        },
-      })
-      .then((success) => {
-        console.log(success);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   public async sendVerificationEmail(to: string, token: string, type: string) {
     try {
       const response = await this.sendGridMail({
         to,
-        from: 'se201027@dsu.edu.pk',
+        from: process.env.MAIL_FROM,
         subject:
           type === 'internal'
             ? 'Registration: Alumni Verification'
@@ -134,35 +50,22 @@ export class MailService {
 
   public async sendPasswordConfirmationEmail(to: string, token: string) {
     try {
-      await this.setTransport();
-    } catch (err) {
-      console.log(err);
-      console.log('Email HALT');
-      return;
-    }
-    this.mailerService
-      .sendMail({
-        transporterName: 'gmail',
-        to: 'se201003@dsu.edu.pk,gosaad@outlook.com', //to,
-        from: 'DSU Alumni Portal <no-reply@dsu.edu.pk>',
+      const response = await this.sendGridMail({
+        to,
+        from: process.env.MAIL_FROM,
         subject: 'Password Change Request Confirmation',
-        template: 'AlumniPasswordChange', // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
-        context: {
-          // Data to be sent to template engine.
+        templateId: PASSWORD_CHANGE_TEMPLATE,
+        dynamicTemplateData: {
           token,
           username: 'Alumni Name',
           domain: process.env.FRONT_DOMAIN || 'https://alumni.dsu.edu.pk',
           link: process.env.FORGOT_PASS_PATH,
         },
-      })
-      .then((success) => {
-        console.log(success);
-      })
-      .catch((err) => {
-        console.log(err);
       });
+
+      this.logger.log(response);
+    } catch (error) {
+      this.logger.error(error);
+    }
   }
 }
-
-// <h1>Hello {{code}}</h1>
-// <p>This is my email template - Saad {{username}}</p>
