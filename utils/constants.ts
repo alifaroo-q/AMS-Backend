2527c3fc51a8bb21de55aa40dcb30861ff807ed3
Ali Farooq <alifarooq122@gmail.com>
2024-05-30 16:32:19 +0500

    changed event image folder and fixed relative path

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
   UPLOAD_LOCATION: './files/alumni/',
-  EVENT_UPLOAD_LOCATION: './files/events',
+  EVENT_UPLOAD_LOCATION: './files/event',
   DEFAULT_AVATAR: 'default/avatar.jpg',
   DEFAULT_EVENT: 'event-placeholder.png',
 };
