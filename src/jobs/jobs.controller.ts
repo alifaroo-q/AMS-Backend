4a758e5e992583901499baed5c706d2e708a1f31
Ali Farooq <alifarooq122@gmail.com>
2024-06-05 12:22:54 +0500

    added swagger docs for job resource

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -12,12 +12,19 @@ import {
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthUser } from '../auth/decorator/user.decorator';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { IAuthUser } from '../auth/types/auth-user.type';
 import { Serialize } from '../../utils/serialize.interceptor';
 import { JobDto } from './dto/job.dto';
+import { Job } from './entities/job.entity';
 
 @ApiTags('Jobs')
 @Controller('jobs')
@@ -26,6 +33,7 @@ export class JobsController {
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiCreatedResponse({ description: 'Job Created', type: Job })
   @Serialize(JobDto)
   create(
     @Body() createJobDto: CreateJobDto,
@@ -44,22 +52,37 @@ export class JobsController {
   }
 
   @Get()
+  @ApiOkResponse({
+    description: 'All Jobs',
+    type: [Job],
+  })
   findAll() {
     return this.jobsService.findAll();
   }
 
   @Get('/unapproved')
+  @ApiOkResponse({
+    description: 'All unapproved jobs',
+    type: [Job],
+  })
   findAllUnApproved() {
     return this.jobsService.findAllUnApproved();
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Job with provided Id', type: Job })
+  @ApiNotFoundResponse({ description: 'Job with provided not found' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.jobsService.findOne(id);
   }
 
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
+  @ApiCreatedResponse({ description: 'Job with provided Id updated' })
+  @ApiForbiddenResponse({ description: 'User not authorized to update jobs' })
+  @ApiNotFoundResponse({
+    description: 'Job with provided Id not found, update failed',
+  })
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateJobDto: UpdateJobDto,
@@ -70,6 +93,8 @@ export class JobsController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
+  @ApiOkResponse({ description: 'Job with provided Id deleted' })
+  @ApiForbiddenResponse({ description: 'User not authorized to delete jobs' })
   remove(
     @Param('id', ParseIntPipe) id: number,
     @AuthUser() currentUser: IAuthUser,
