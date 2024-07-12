f33c98f0c67822a22fd762c1d2310ede7c1a6815
Ali Farooq <alifarooq122@gmail.com>
2024-07-12 15:12:14 +0500

    changed survey data types again

--- src/survey/dto/update-survey.dto.ts
+++ src/survey/dto/update-survey.dto.ts
@@ -141,35 +141,35 @@ export class UpdateSurveyDto extends PartialType(CreateSurveyDto) {
     example: '1',
   })
   @IsNotEmpty()
-  q20: number;
+  q20: string;
 
   @ApiProperty({
     description: 'Choice Question (1 to 5)',
     example: '1',
   })
   @IsNotEmpty()
-  q21: number;
+  q21: string;
 
   @ApiProperty({
     description: 'Choice Question (1 to 5)',
     example: '1',
   })
   @IsNotEmpty()
-  q22: number;
+  q22: string;
 
   @ApiProperty({
     description: 'Choice Question (1 to 5)',
     example: '1',
   })
   @IsNotEmpty()
-  q23: number;
+  q23: string;
 
   @ApiProperty({
     description: 'Choice Question (1 to 5)',
     example: '1',
   })
   @IsNotEmpty()
-  q24: number;
+  q24: string;
 
   @ApiProperty({
     description: 'Custom string',
@@ -191,153 +191,4 @@ export class UpdateSurveyDto extends PartialType(CreateSurveyDto) {
   })
   @IsNotEmpty()
   q27: string;
-
-  @ApiProperty({
-    description: 'Choice Question (A to F)',
-    example: 'A',
-  })
-  @IsNotEmpty()
-  q28: string;
-
-  @ApiProperty({
-    description: 'Choice Question (A to F)',
-    example: 'A',
-  })
-  @IsNotEmpty()
-  q29: string;
-
-  @ApiProperty({
-    description: 'Choice Question (A to F)',
-    example: 'A',
-  })
-  @IsNotEmpty()
-  q30: string;
-
-  @ApiProperty({
-    description: 'name',
-    example: 'Saad',
-  })
-  @IsNotEmpty()
-  name: string;
-
-  @ApiProperty({
-    description: 'registrationNo',
-    example: 'SE232223',
-  })
-  @IsNotEmpty()
-  registrationNo: string;
-
-  @ApiProperty({
-    description: 'yearOfIntake',
-    example: '2020',
-  })
-  @IsNotEmpty()
-  yearOfIntake: string;
-
-  @ApiProperty({
-    description: 'degreeProgram',
-    example: 'SE',
-  })
-  @IsNotEmpty()
-  degreeProgram: string;
-
-  @ApiProperty({
-    description: 'yearOfGraduation',
-    example: '2024',
-  })
-  @IsNotEmpty()
-  yearOfGraduation: string;
-
-  @ApiProperty({
-    description: 'employed',
-    example: 'No',
-  })
-  @IsNotEmpty()
-  employed: string;
-
-  @ApiProperty({
-    description: 'current Employer',
-    example: 'Avanza',
-  })
-  currentEmployer: string;
-
-  @ApiProperty({
-    description: 'position',
-    example: 'Sr. Developer',
-  })
-  position: string;
-
-  @ApiProperty({
-    description: 'industry',
-    example: 'Information Technology',
-  })
-  industry: string;
-
-  @ApiProperty({
-    description: 'employmentPeriod1',
-    example: '2020-12-25 05:00:00',
-  })
-  employmentPeriod1: Date;
-
-  @ApiProperty({
-    description: 'employmentPeriod2',
-    example: '2020-12-25 05:00:00',
-  })
-  employmentPeriod2: Date;
-
-  @ApiProperty({
-    description: 'lastSalary',
-    example: '240000',
-  })
-  lastSalary: number;
-
-  @ApiProperty({
-    description: 'academicSpecialization',
-    example: 'Software Engineer',
-  })
-  academicSpecialization: string;
-
-  @ApiProperty({
-    description: 'professionalSpecialization',
-    example: 'Software Engineer',
-  })
-  professionalSpecialization: string;
-
-  @ApiProperty({
-    description: 'firstJobExperienceYear',
-    example: '2000',
-  })
-  firstJobExperienceYear: string;
-
-  @ApiProperty({
-    description: 'firstJobEmploymentPeriod1',
-    example: '2020-12-25 05:00:00',
-  })
-  firstJobEmploymentPeriod1: Date;
-
-  @ApiProperty({
-    description: 'firstJobEmploymentPeriod2',
-    example: '2020-12-25 05:00:00',
-  })
-  firstJobEmploymentPeriod2: Date;
-
-  @ApiProperty({
-    description: 'firstJobSalary',
-    example: '24000',
-  })
-  firstJobSalary: number;
-
-  @ApiProperty({
-    description: 'personalEmail',
-    example: 'ab@gmail.com',
-  })
-  @IsNotEmpty()
-  personalEmail: string;
-
-  @ApiProperty({
-    description: 'mobileContact',
-    example: '033392382292',
-  })
-  @IsNotEmpty()
-  mobileContact: string;
 }
